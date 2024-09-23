d03bd912f48f08a22b48aa6a3a86bd3a4ff1c014
Fidelia <mochihypnly@gmail.com>
2024-09-23 09:58:02 +0700

    Update hitung.py

--- hitung.py
+++ hitung.py
@@ -1,9 +1,8 @@
 import streamlit as st
 
-x1 = st.number_input("Masukkan angka 1")
-sx1 = st.text_input("Satuan 1", "C")
-st.write ("Anda memasukkan", x1, ' ', sx1)
+x = st.number_input("Masukkan angka")
+sx = st.text_input("Satuan", "C")
+st.write ("Anda memasukkan", x, ' ', sx)
+sy = st.text_input("Dikonversi ke"' "C")
 
-x2 = st.number_input("Masukkan angka 2")
-s2 = st.text_input("Satuan 2", "C")
-st.write ("Anda memasukkan", x2, ' ', sx2)
+st.write (x, ' ', sx, '= ...', sy)
