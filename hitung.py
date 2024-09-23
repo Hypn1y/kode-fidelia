18a973ee8327985b9e4e9a2a0755ae7afafd0662
Fidelia <mochihypnly@gmail.com>
2024-09-23 09:47:12 +0700

    Update hitung.py

--- hitung.py
+++ hitung.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 x = st.number_input(
-  "Insert a number", value=None, placehlder="Tuliskan angka..."
+  "Insert a number", value=None, placeholder="Tuliskan angka..."
   )
 st.write('the current number is ", x)
 st.latex(r'''
