cd0cee67cd527c210b1617424382dd9675844f21
Fidelia <mochihypnly@gmail.com>
2024-09-23 09:52:50 +0700

    Update hitung.py

--- hitung.py
+++ hitung.py
@@ -3,3 +3,7 @@ import streamlit as st
 x = st.number_input("Masukkan angka")
 sx = st.text_input("Satuan", "C")
 st.write ("Anda memasukkan", x, ' ', sx)
+
+y = st.number_input("Masukkan angka")
+sy = st.text_input("Satuan", "C")
+st.write ("Anda memasukkan", y, ' ', sy)
