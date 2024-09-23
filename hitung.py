ebfdf2b03fdc94c647dcf136b2f2798f7aed3c19
Fidelia <mochihypnly@gmail.com>
2024-09-23 10:14:44 +0700

    Update hitung.py

--- hitung.py
+++ hitung.py
@@ -9,4 +9,23 @@ if (sx == 'C'):
   if(sy == 'C'):
     y = x
   elif(sy == 'F'):
+    y = (x * 9/5) + 32
+  elif sy == 'K'
+     y = x + 273.15
+
+elif sx == 'F':  
+    if sy == 'C':  
+        y = (x - 32) * 5/9
+    elif sy == 'F':  
+        y = x
+    elif sy == 'K':  
+        y = (x - 32) * 5/9 + 273.15
+
+elif sx == 'K':  
+    if sy == 'C':  
+        y = x - 273.15
+    elif sy == 'F': 
+        y = (x - 273.15) * 9/5 + 32
+    elif sy == 'K':  
+        y = x
 st.write (x, ' ', sx, ' = ', y , sy)
