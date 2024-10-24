076923a364a9a8d6aa658adf605e0947d1f264de
Fidelia <mochihypnly@gmail.com>
2024-10-24 19:02:44 +0700

    Update uts.py

--- uts.py
+++ uts.py
@@ -7,7 +7,7 @@ from matplotlib.patches import Circle
 
 st.title("Fisika Komputasi Awan")
 st.title("Enrico Frans Ganya")
-circle = Circle((0, 0), 1, color='red', fill=False, linewidth=2, linestyle='-', alpha=0.5)
+circle = Circle((0, 0), 1, color='red', fill=False, linewidth=2, linestyle='-', alpha=0.2)
 x = []
 y = []
 color = []
@@ -35,9 +35,9 @@ fig, ax = plt.subplots(figsize=(16, 16))
 ax.add_patch(circle)
 
 for i in range(1, len(x)):
-    ax.plot([0, x[i]], [0, y[i]], color='green', linestyle='--', alpha=0.5)
+    ax.plot([0, x[i]], [0, y[i]], color='green', linestyle='--', alpha=0.2)
 
-ax.scatter(x, y, c=color, s=size, alpha=0.5) 
+ax.scatter(x, y, c=color, s=size, alpha=0.6) 
 
 ax.set_ylabel("y")
 ax.set_xlabel("x")
