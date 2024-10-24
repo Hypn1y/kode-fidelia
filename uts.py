f8ca1825ff17146bbfa40382b91d31e8ef0921e4
Fidelia <mochihypnly@gmail.com>
2024-10-24 09:21:10 +0700

    Update uts.py

--- uts.py
+++ uts.py
@@ -1,29 +1,54 @@
 import streamlit as st
-import matplotlib.pyplot as plt
+import random
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
 
-# Fungsi untuk menghasilkan data lingkaran acak
-def generate_random_circle_data():
-    num_circles = 50
-    angles = np.linspace(0, 2 * np.pi, num_circles)
-    radii = np.random.rand(num_circles)
-    sizes = np.random.rand(num_circles) * 300  # Ukuran lingkaran acak
-    return angles, radii, sizes
 
-# Header aplikasi
-st.title("Lingkaran Acak yang Berubah Setiap Kali Tombol Ditekan 😄")
+st.title("Fisika Komputasi Awan")
+st.title("Ganya")
+circle = Circle((0, 0), 1, color='red', fill=False, linewidth=2, linestyle='-', alpha=0.2)
+x = []
+y = []
+color = []
+size = []
+x.append(0)
+y.append(0)
+color.append((0.,.7,0.))
+size.append(371)
+if st.button("Data"):
+    for i in range(111):
+        x0 = 2*(random.random() - .5)
+        y0 = 2*(random.random() - .5)
+        if ((x0*2+y0*2)>1.):
+            if y0>0:
+                y0 = np.sqrt(1-x0**2)
+            else:
+                y0 = -1*(np.sqrt(1-x0**2))
+            
+        x.append(x0)
+        y.append(y0)
+        color.append((random.random(),random.random(),random.random()))
+        size.append(3713*random.random() )
+    
+fig, ax = plt.subplots(figsize=(16, 16))
+ax.add_patch(circle)
 
-# Tombol untuk memperbarui data
-if st.button('Data'):
-    angles, radii, sizes = generate_random_circle_data()
+for i in range(1, len(x)):
+    ax.plot([0, x[i]], [0, y[i]], color='green', linestyle='--', alpha=0.2)
 
-    fig, ax = plt.subplots()
-    ax.set_xlim([-1, 1])
-    ax.set_ylim([-1, 1])
+ax.scatter(x, y, c=color, s=size, alpha=0.5) 
 
-    # Membuat plot lingkaran acak
-    fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
-    ax.scatter(angles, radii, s=sizes, alpha=0.5)
-
-    # Menampilkan plot
-    st.pyplot(fig)
+ax.set_ylabel("y")
+ax.set_xlabel("x")
+ax.tick_params(axis='y', labelsize=20)
+#ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha='right')
+ax.tick_params(axis='x', labelsize=15)
+ax.set_title('Data Acak yang berubah setiap tombol ditekan')
+ax.grid(True, linestyle='-.')
+ax.tick_params(labelcolor='r', labelsize='medium', width=3)
+ax.set_xlim([-1, 1])
+ax.set_ylim([-1, 1])
+st.pyplot(fig)
+st.caption("Lingkaran dengan ukuran dan warna acak dan tersebar didalam lingkaran dengan radius 1")
+st.divider()
